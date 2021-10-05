a4f8603c5afc97b0fe010f2cb0b3b80a3e21a087
Drazen Drinic <drazendrinic@hotmail.com>
2021-10-05 11:16:21 +0200

    Finished
    Made a code to make the game work

--- Guess The Numbah Game/Assets/Scripts/GuessTheNumberGame.cs
+++ Guess The Numbah Game/Assets/Scripts/GuessTheNumberGame.cs
@@ -5,10 +5,36 @@ using UnityEngine.UI;
 
 public class GuessTheNumberGame : MonoBehaviour
 {
+    public InputField input;
+    public Text infoText;
+
+    private int GuessNumber;
+    private int userGuess;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GuessNumber = Random.Range(0, 100);
+    }
+
+    public void CheckGuess()
+    {
+        userGuess = int.Parse(input.text);
+
+        if (userGuess == GuessNumber)
+        {
+            infoText.text = "You guessed the number, it is " + userGuess;
+        }
+        else if (userGuess > GuessNumber)
+        {
+            infoText.text = "You guess is "+ userGuess + " and it is grater than the guessed number";
+        }
+        else if (userGuess < GuessNumber)
+        {
+            infoText.text = "You guess is " + userGuess + " and it is lower than the guessed number";
+        }
+
+        input.text = "";
     }
 
     // Update is called once per frame
